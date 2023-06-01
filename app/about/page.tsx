ada37d69e1411e171ec447df16a85950f9c0669a
Qasim Ali <qasim.ali1@vodafone.com>
2023-06-01 15:26:49 -0400

    about added

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import CS2Header from "@/app/sections/header/header";
+import CS2Content from "@/app/sections/content/content";
+
+export default function about() {
+    return (
+        <div>
+           About CS2 can be defined as CS2
+            <CS2Header></CS2Header>
+            <CS2Content></CS2Content>
+        </div>
+    )
+}
