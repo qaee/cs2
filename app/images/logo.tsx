2ee9210977e546e5296e48e286909a998021b4de
Qasim Ali <qasim.ali1@vodafone.com>
2023-06-01 16:25:28 -0400

    logo added

---
+++ app/images/logo.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+export default function CS2Logo() {
+
+    return (
+        <Image src="/images/logo.png" alt="me" width="100" height="100" />
+    )
+}
+
--- app/sections/header/header.tsx
+++ app/sections/header/header.tsx
@@ -1,5 +1,10 @@
+import CS2Logo from "@/app/images/logo";
+
 export default function CS2Header() {
     return (
-        <h1> CS2 Header</h1>
+        <div>
+           <CS2Logo></CS2Logo>
+            <h1> CS2 Header</h1>
+        </div>
     )
 }
