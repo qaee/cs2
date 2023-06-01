7722f483245ee102af6e0b91a0cf879cc3c01e53
Qasim Ali <qasim.ali1@vodafone.com>
2023-06-01 17:24:19 -0400

    material ui added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 import CS2Header from "@/app/sections/header/header";
 import CS2Content from "@/app/sections/content/content";
-
+import { Button, Grid, Stack } from "@mui/material";
 export default function about() {
     return (
         <div>
-           About CS2 can be defined as CS2
-            <CS2Header></CS2Header>
-            <CS2Content></CS2Content>
+        <Grid container height="100vh" alignItems="center" justifyContent="center" direction="column">
+            <h1>Using Material UI with Next.js 13</h1>
+            <Stack direction="row" columnGap={1}>
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+            </Stack>
+        </Grid>
+        <CS2Content></CS2Content>
         </div>
     )
 }
