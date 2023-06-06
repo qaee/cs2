a295fd275df7649cfbb235e2e828c6a3bb50b02a
Qasim Ali <qasim.ali1@vodafone.com>
2023-06-05 22:26:19 -0400

    design made responsive

--- app/sections/content/content.tsx
+++ app/sections/content/content.tsx
@@ -1,22 +1,10 @@
-"use client";
 import exp from "constants";
-import {Button, Typography} from "@mui/material";
+import {Button, Typography, Grid} from "@mui/material";
 export default function CS2Content () {
     return (
         <>
-                <Typography variant="subtitle1">subtitle1</Typography>
-                <Typography variant="subtitle2">subtitle2</Typography>
-                <Typography variant="button">button</Typography>
-                <Typography variant="caption">caption</Typography>
-                <Typography variant="h1">h1</Typography>
-                <Typography variant="h2">h2</Typography>
-                <Typography variant="h3">h3</Typography>
-                <Typography variant="h4">h4</Typography>
-                <Typography variant="h5">h5</Typography>
-                <Typography variant="h6">h6</Typography>
-                <Typography variant="body2">
-                    7
-
+            <Grid >
+                <Typography variant="body1">
                     I'm currently designing and building a job board website.
 
                     I've started designing the page which shows a full job description posting once the user clicks through from a list of search results.
@@ -28,14 +16,7 @@ export default function CS2Content () {
                     What methods can I use to layout or style this kind of thing to make it look interesting, readable and not just seem like a dull word document whilst maintaining a professional look?
                     Body 2
                 </Typography>
-                <Typography variant="body1">
-                    Body 1
-                    Body 1
-                    Body 1
-                    Body 1
-                    Body 1
-                </Typography>
-            HI
+            </Grid>
         </>
 
     )
--- app/sections/cs2nav/Cs2Nav.tsx
+++ app/sections/cs2nav/Cs2Nav.tsx
@@ -1,36 +1,27 @@
 "use client";
 import AppBar from '@mui/material/AppBar';
-import {Button, Container, Grid, IconButton, Toolbar, Typography} from "@mui/material";
-import {Badge} from "@mui/base";
+import {Button, Container, Box,Grid, IconButton, Toolbar, Typography} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import { AccessAlarm, ThreeDRotation  } from '@mui/icons-material';
 import Link from "next/link";
-import React from 'react'
+
 import CS2Logo from "@/app/images/logo";
-import { Roboto } from 'next/font/google';
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
-
-export const roboto = Roboto({
-    weight: ['300', '400', '500', '700'],
-    subsets: ['latin'],
-    display: 'swap',
-    fallback: ['Helvetica', 'Arial', 'sans-serif'],
-});
-
+import {right} from "@popperjs/core";
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 export default function Cs2Nav() {
     return (
         <>
-            <Grid container spacing={2} >
-                <Grid item xs={2}>
-                <CS2Logo />
-                </Grid>
-                <Grid item xs={8}>
-                    <Button color="inherit" component={Link} href="/register">Link1</Button>
-                    <Button color="inherit" component={Link} href="/register">Link2</Button>
-                    <Button color="inherit" component={Link} href="/register">Link3</Button>
-                    <Button color="inherit" component={Link} href="/register">Link4</Button>
-                    <Button color="inherit" component={Link} href="/register">Link5</Button>
+            <Grid container sx={{border: 2 }} >
+                <Grid item sx={{border: 1,m:3 }} xs={2}>
+                    <CS2Logo></CS2Logo>
+                 </Grid>
+                <Grid sx={{display: 'flex', border: 2,borderColor: 'error.main',m:3, justifyContent: 'flex-end' }} xs={8}>
+                        <Button color="inherit" component={Link} href="/about">Services</Button>
+                        <Button color="inherit" component={Link} href="/contact">Partners</Button>
+                        <Button color="inherit" component={Link} href="/about">About</Button>
+                        <Button color="inherit" component={Link} href="/contact">contact </Button>
                 </Grid>
             </Grid>
         </>
